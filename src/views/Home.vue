c708b31d19c0def44669ec3407ebc6179bdaf89e
MichaelHo <michelo851a1203@gmail.com>
2020-09-07 20:02:26 +0800

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input class="border px-2 py-1 border-gray-600 rounded" type="text" />
+    <div class="calenderBorder">
+      <div class="w-full flex item-centers justify-center">
+        <div class="mx-1 rounded" v-for="item in week" :key="item">{{ item }}</div>
+      </div>
+      <div v-for="row in 5" :key="row">
+
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { ref } from "vue";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {},
+  setup() {
+    const week = ref(["Sun.", "Mon.", "Tue.", "Wed.", "Thu.", "Fri.", "Sat."]);
+    console.log(cDate);
+    
+    return { week };
+  },
+};
 </script>
+
+<style lang="postcss">
+.calenderBorder {
+  @apply px-3 py-2 rounded bg-blue-300 h-64;
+  max-width: 18rem;
+}
+</style>
