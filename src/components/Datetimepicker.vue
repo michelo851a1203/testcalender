010b1cbb3abfc3a9d2468db898af6a04cf9c6027
MichaelHo <michelo851a1203@gmail.com>
2020-09-09 14:08:55 +0800

    20200908 append all solutuon

--- src/components/Datetimepicker.vue
+++ src/components/Datetimepicker.vue
@@ -1,60 +1,69 @@
 <template>
   <div>
-    <input class="border px-2 py-1 border-gray-600 rounded" type="text" />
-    <div class="calenderBorder">
-      <div class="flex items-center justify-around mb-2">
-        <div class="flex items-center select-none">
-          <div class="cursor-pointer" @click="switchYear('prev')">
-            <svg class="w-4" viewBox="0 0 100 100">
-              <polyline
-                points="65 20 30 50 65 80"
-                stroke-width="10"
-                stroke="black"
-                fill="transparent"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
+    <input
+      :value="maintext"
+      @focus="isHoverCalender(true)"
+      @blur="isHoverCalender(false)"
+      class="border px-2 py-1 border-gray-600 rounded"
+      type="text"
+    />
+    <transition name="fade">
+      <div v-if="isShowCalender" class="calenderBorder">
+        <div class="flex items-center justify-around mb-2">
+          <div class="flex items-center select-none">
+            <div class="cursor-pointer" @click="switchYear('prev')">
+              <svg class="w-4" viewBox="0 0 100 100">
+                <polyline
+                  points="65 20 30 50 65 80"
+                  stroke-width="10"
+                  stroke="black"
+                  fill="transparent"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+            </div>
+            <div class="select-none rounded px-2 py-1 shadow-lg mx-4">{{ selectYearRef }}</div>
+            <div class="cursor-pointer" @click="switchYear('next')">
+              <svg class="w-3" viewBox="0 0 100 100">
+                <polyline
+                  points="25,10 80,50 25,90"
+                  stroke="black"
+                  stroke-width="16"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  fill="transparent"
+                />
+              </svg>
+            </div>
           </div>
-          <div class="select-none rounded px-2 py-1 shadow-lg mx-4">{{ selectYearRef }}</div>
-          <div class="cursor-pointer" @click="switchYear('next')">
-            <svg class="w-3" viewBox="0 0 100 100">
-              <polyline
-                points="25,10 80,50 25,90"
-                stroke="black"
-                stroke-width="16"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                fill="transparent"
-              />
-            </svg>
+          <div>
+            <label class="mr-2" for>month</label>
+            <select v-model="selectMonthRef" class="border rounded border-gray-500 px-2 py-1">
+              <option v-for="item in 12" :key="item" class="bg-white" :value="item">{{ item }}</option>
+            </select>
           </div>
         </div>
-        <div>
-          <label class="mr-2" for>month</label>
-          <select v-model="selectMonthRef" class="border rounded border-gray-500 px-2 py-1">
-            <option v-for="item in 12" :key="item" class="bg-white" :value="item">{{ item }}</option>
-          </select>
-        </div>
+        <table class="w-full">
+          <thead>
+            <tr>
+              <th class="px-1" v-for="item in week" :key="item">{{ item }}</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="row in setrowRef" :key="row">
+              <td class="text-center" v-for="col in week.length" :key="col">
+                <div
+                  @click="dateClick(allSetRef[(col - 1) + (row - 1) * week.length])"
+                  class="mx-auto rounded-full w-6 h-6 transition duration-300 ease-in-out hover:bg-blue-300 hover:shadow-2xl cursor-pointer"
+                  :class="{ 'bg-red-300': currentDateRef === allSetRef[(col - 1) + (row - 1) * week.length] && currentYearRef === selectYearRef && currentMonthRef === selectMonthRef }"
+                >{{ allSetRef[(col - 1) + (row - 1) * week.length] }}</div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
-      <table class="w-full">
-        <thead>
-          <tr>
-            <th class="px-1" v-for="item in week" :key="item">{{ item }}</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="row in setrowRef" :key="row">
-            <td class="text-center" v-for="col in week.length" :key="col">
-              <div
-                class="mx-auto rounded-full w-6 h-6 transition duration-300 ease-in-out hover:bg-blue-300 hover:shadow-2xl cursor-pointer"
-                :class="{ 'bg-red-300': currentDateRef === allSetRef[(col - 1) + (row - 1) * week.length] && currentYearRef === selectYearRef && currentMonthRef === selectMonthRef }"
-              >{{ allSetRef[(col - 1) + (row - 1) * week.length] }}</div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+    </transition>
   </div>
 </template>
 
@@ -62,18 +71,28 @@
 import { ref, watch } from "vue";
 export default {
   name: "datetimepicker",
-  setup() {
+  props: {
+    maintext: {
+      type: String,
+      default: "",
+    },
+  },
+  setup(props, { emit }) {
     const week = ref(["Sun.", "Mon.", "Tue.", "Wed.", "Thu.", "Fri.", "Sat."]);
     // get days of month
     const current = new Date();
 
+    const isShowCalender = ref(false);
     const currentYearRef = ref(current.getFullYear());
     const currentMonthRef = ref(current.getMonth() + 1);
     const currentDateRef = ref(current.getDate());
 
     const selectYearRef = ref(current.getFullYear());
     const selectMonthRef = ref(current.getMonth() + 1);
-    const selectDateRef = ref(current.getDate());
+
+    const isHoverCalender = (hover) => {
+      isShowCalender.value = hover;
+    };
 
     const changeContent = (weekLen, chooseYear, chooseMonth) => {
       const numOfPreDate = new Date(chooseYear, chooseMonth - 1, 0).getDate();
@@ -112,6 +131,8 @@ export default {
         allSet = [...allSet, ...lastLen];
       }
 
+      // this place need to change data format solution.
+
       return {
         firstDayWeek,
         setrow,
@@ -128,6 +149,16 @@ export default {
         selectYearRef.value = selectYearRef.value + 1;
         return;
       }
+    };
+
+    const dateClick = (iData) => {
+      const mainMonth =
+        `${selectMonthRef.value}`.length === 1
+          ? `0${selectMonthRef.value}`
+          : `${selectMonthRef.value}`;
+      const mainDate = `${iData}`.length === 1 ? `0${iData}` : `${iData}`;
+      const dateFmt = `${selectYearRef.value}-${mainMonth}-${mainDate}`;
+      emit("update:maintext", dateFmt);
     };
 
     const { setrow, allSet } = changeContent(
@@ -161,10 +192,12 @@ export default {
     });
 
     return {
+      isShowCalender,
+      isHoverCalender,
       switchYear,
+      dateClick,
       selectYearRef,
       selectMonthRef,
-      selectDateRef,
       currentYearRef,
       currentMonthRef,
       currentDateRef,
@@ -180,5 +213,13 @@ export default {
 .calenderBorder {
   @apply py-4 rounded bg-gray-300 shadow-xl;
   max-width: 20rem;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease-out;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="home">
-    <datetimepicker></datetimepicker>
+    <datetimepicker v-model:maintext="mainDataRef"></datetimepicker>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import datetimepicker from "@/components/Datetimepicker.vue";
+import { ref } from "vue"
 export default {
   name: "Home",
   components: {
     datetimepicker,
+  },
+  setup() {
+    const mainDataRef = ref("");
+    return { mainDataRef };
   },
 };
 </script>
